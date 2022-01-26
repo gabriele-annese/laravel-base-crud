64fba5f82e11b4f456f7678b316d2c8e067e5c76
gabriele-annese <gannese06@gmail.com>
2022-01-26 15:11:07 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container">
+        <h1 class="text-center">Welcome Home</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui at, accusantium illum optio minus corrupti quasi eaque tenetur officia? Quas, eligendi? Et delectus consectetur iusto porro ut iste neque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ad, in eaque vero inventore tempore voluptates distinctio hic quis officia suscipit! Natus placeat quia quibusdam totam iste ratione impedit quas.
+        </p>
+    </div>
+@endsection
